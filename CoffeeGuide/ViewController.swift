59929ccfddd058dd90eb77cd086c6523116994ab
張嘉恬 <karolchang@karols.local>
2016-03-30 20:53:58 +0800

    Initial Commit

---
+++ CoffeeGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeGuide
+//
+//  Created by 張嘉恬 on 2016/3/30.
+//  Copyright © 2016年 張嘉恬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
